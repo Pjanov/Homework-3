41cd2cf21f21677ac1049c2bc04060f0b82f2786
Андрей <pjanov84@mail.ru>
2022-10-15 17:42:17 +0300

    Решение к задачи_5

--- Task_5.py
+++ Task_5.py
@@ -5,5 +5,19 @@
 # Алгоритм смотрим тут: https://ru.wikipedia.org/wiki/Негафибоначчи
 # Вам понадобится рекурсивный вызов функции или сделайте в виде списка.
 
+def get_fibonacci(n):
+    fibonacci_list = []
+    a, b = 1, 1
+    for i in range(n):
+        fibonacci_list.append(a)
+        a, b = b, a + b
+    a, b = 0, 1
+    for i in range(n + 1):
+        fibonacci_list.insert(0, a)
+        a, b = b, a - b
+    return fibonacci_list
 
 
+k = 8
+fibonacci_list = get_fibonacci(k)
+print(f'для k = {k} список будет выглядеть так: {fibonacci_list}')
