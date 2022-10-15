60e9473324b1dc5753ef8d235799d6dd3d74ace7
Андрей <pjanov84@mail.ru>
2022-10-15 17:41:23 +0300

    Решение к задачи_4

--- Task_4.py
+++ Task_4.py
@@ -5,10 +5,12 @@
 # 2 -> '10'
 # Примечание: Результат вернуть в виде строки. Не использовать функции преобразования типов: bin
 
+decimal_number = 45
+binary_number = ''
 
+print(decimal_number, '->', end=' ')
 
-
-
-
-
-
+while decimal_number != 0:
+    binary_number = str(decimal_number % 2) + binary_number
+    decimal_number //= 2
+print("'{}'".format(binary_number))
